2ff882502b1caef4093f4ac2b2df4cf66b85671e
Joseph Rios Henao <joseph232005@gmail.com>
2021-04-01 12:40:09 -0500

    finish forms component and validation

--- practice4/src/app/formulario/formulario.component.ts
+++ practice4/src/app/formulario/formulario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Egreso } from '../egreso/egreso.model';
+import { EgresoServicio } from '../egreso/egreso.service';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { IngresoServicio } from '../ingreso/ingreso.service';
 
 @Component({
   selector: 'app-formulario',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
-  constructor() { }
+
+  type:string = "ingresoOperation";
+  description:string = "";
+  value:number = 0;
+
+  constructor(private ingresoServicio:IngresoServicio,
+              private egresoServicio:EgresoServicio) { }
 
   ngOnInit(): void {
   }
+  
+  typeOperation(evento:any){
+    this.type = evento.target.value;
+  }
 
+  addValue(){
+    if(this.type==="ingresoOperation"){
+      this.ingresoServicio.ingresos.push(new Ingreso(this.description,this.value));
+    }else{
+      this.egresoServicio.egresos.push(new Egreso(this.description,this.value));
+    }
+  }
 }
