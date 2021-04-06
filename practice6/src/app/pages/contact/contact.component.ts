04b573fd3245f143c46788d170c094df93ecf909
Joseph Rios Henao <joseph232005@gmail.com>
2021-04-06 12:52:51 -0500

    form reactive finish and feedback.. continue with video

--- practice6/src/app/pages/contact/contact.component.ts
+++ practice6/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  contactForm:FormGroup;
+  private isEmail: string | RegExp = '^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$';
+  constructor( private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  onSave(){
+    if(this.contactForm.valid){
+      console.log(this.contactForm.value);
+    }else{
+      console.log('Not Valid');
+      console.log(this.contactForm.value);
+    }
+  }
+
+  isValidField(field:string):string {
+    const validateField = this.contactForm.get(field);  
+    return (!validateField.valid && validateField.touched)
+    ? "is-invalid" : validateField.touched ? "is-valid" : "";
+  }
+
+  private initForm():void{
+    this.contactForm = this.fb.group({
+      name: ['',[Validators.required]],
+      lastName: [''],
+      email: ['',[Validators.required,Validators.pattern(this.isEmail)]],
+      message: ['',[Validators.required, Validators.minLength(10), Validators.maxLength(200)]],
+    })
+  }
 }
