370c35adc526b5b88da395133b2a189710fe4d5f
Joseph Rios Henao <joseph232005@gmail.com>
2021-04-11 11:28:45 -0500

    configure routing path and app component html

--- practice5/src/app/app-routing.module.ts
+++ practice5/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { TableComponent } from './components/table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TableComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'client/edit/:id', component: EditClientComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
