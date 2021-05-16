61acf9da94bb752acccc709cc8a641e4a1ddbe5d
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-16 15:42:20 -0500

    add route in routing and navbar

--- practice10/src/app/app-routing.module.ts
+++ practice10/src/app/app-routing.module.ts
@@ -4,42 +4,24 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { HelpComponent } from './help/help.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductComponent } from './product/product.component';
 import { AddUserComponent } from './users/add-user/add-user.component';
 import { EditUserComponent } from './users/edit-user/edit-user.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
-  {
-    path : "",
-    component: HomeComponent
-  },
-  {
-    path : "about-me",
-    component: AboutMeComponent
-  },
-  {
-    path : "help",
-    component: HelpComponent
-  },
-  {
-    path : "users",
-    component: UsersComponent,
+  { path : "",         component : HomeComponent     },
+  { path : "about-me", component : AboutMeComponent  },
+  { path : "help",     component : HelpComponent     },
+  { path : "users",    component : UsersComponent,
     children:
     [
-      {
-        path : "add",
-        component: AddUserComponent
-      },
-      {
-        path : "edit",
-        component: EditUserComponent
-      }
+      { path : "add",  component : AddUserComponent  },
+      { path : "edit", component : EditUserComponent },
     ],
   },
-  {
-    path : "**",
-    component: NotFoundComponent
-  }
+  { path : "product",  component : ProductComponent  },
+  { path : "**",       component : NotFoundComponent },
 ];
 
 @NgModule({
