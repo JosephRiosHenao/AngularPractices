5940301914f238a4df2f3bec3a58299b15e5f537
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-15 22:11:29 -0500

    create components, continuew with configurate raouting and navbar

--- practice10/src/app/app.module.ts
+++ practice10/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HelpComponent } from './help/help.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HelpComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
