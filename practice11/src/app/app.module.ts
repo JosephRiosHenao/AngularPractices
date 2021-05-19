df36dcd0f4710d15c9074773e52897d29d7a8880
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-19 14:43:37 -0500

    generate form component

--- practice11/src/app/app.module.ts
+++ practice11/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormAddComponent } from './form-add/form-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormAddComponent
   ],
   imports: [
     BrowserModule,
