3965d07f61d25ec36ac6672c4228c7fb560cb520
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-24 23:38:06 -0500

    add form builder

--- practice11/src/app/form-add/form-add.component.ts
+++ practice11/src/app/form-add/form-add.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-add',
   templateUrl: './form-add.component.html',
-  styleUrls: ['./form-add.component.css']
+  styleUrls: ['./form-add.component.css'],
 })
 export class FormAddComponent implements OnInit {
-
-  constructor() { }
+  formCreate!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm() {
+    this.formCreate = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(8)]),
+      ],
+    });
+  }
+
+  submit(){
+    console.log(this.formCreate.value);
+  }
 }
