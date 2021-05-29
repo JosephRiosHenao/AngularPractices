cd265ba69252289594a9cb679561839549a550a8
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-29 18:12:54 -0500

    add new camps in the form

--- practice13/src/app/header-metadata/header-metadata.component.ts
+++ practice13/src/app/header-metadata/header-metadata.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderMetadataComponent implements OnInit {
 
+  public dateStart:string = "";
   constructor() { }
 
   ngOnInit(): void {
