88e8f6f476a8f5ea199b4ffc0c0d41e29f537fbf
Joseph Rios Henao <joseph232005@gmail.com>
2021-05-29 23:21:47 -0500

    init new component

--- practice13/src/app/app.module.ts
+++ practice13/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderMetadataComponent } from './header-metadata/header-metadata.component';
 import { FormsModule } from '@angular/forms';
+import { FormBeforeWorkComponent } from './form-before-work/form-before-work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderMetadataComponent
+    HeaderMetadataComponent,
+    FormBeforeWorkComponent
   ],
   imports: [
     BrowserModule,
---
+++ practice13/src/app/form-before-work/form-before-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-before-work',
+  templateUrl: './form-before-work.component.html',
+  styleUrls: ['./form-before-work.component.scss']
+})
+export class FormBeforeWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
