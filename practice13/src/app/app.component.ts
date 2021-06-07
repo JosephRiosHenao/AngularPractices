6a627c88c99995cf00b2c5ac3c9dbea466c08a2e
Joseph Rios Henao <joseph232005@gmail.com>
2021-06-07 10:45:30 -0500

    alpha with pdfmake

--- practice13/src/app/app.component.ts
+++ practice13/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+import jsPDF from 'jspdf';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import htmlToPdfmake from 'html-to-pdfmake';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  @ViewChild('pdfTable') pdfTable: ElementRef;
+
+  public downloadAsPDF() {
+    const doc = new jsPDF();
+
+    const pdfTable = this.pdfTable.nativeElement;
+
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).open();
+    doc.save("documento");
+
+  }
 }
