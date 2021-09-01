fcc4f871ca3edecbfa994235bf39de63cea511b9
Joseph Rios Henao <joseph232005@gmail.com>
2021-09-01 18:32:57 -0500

    upgrade grid, colors, add page button func and card template

--- practice18/src/app/app.component.ts
+++ practice18/src/app/app.component.ts
@@ -10,13 +10,27 @@ import { APIService } from './services/api.service';
 export class AppComponent {
   title = 'practice18';
   characters:any = {};
-  constructor (private apiService:APIService, http:HttpClient){
-    http.get("https://rickandmortyapi.com/api/character").subscribe( data => {
+  constructor (private apiService:APIService, public http:HttpClient){
+    http.get("https://rickandmortyapi.com/api/character").subscribe( (data:any) => {
       this.characters = data
       console.log("data update!")
       console.log(this.characters)
     } );
+  }
 
+  next() {
+    this.http.get(this.characters.info.next).subscribe( (data:any) => {
+      this.characters = data
+      console.log("data update!")
+      console.log(this.characters)
+    } );
+  }
 
+  prev() {
+    this.http.get(this.characters.info.prev).subscribe( (data:any) => {
+      this.characters = data
+      console.log("data update!")
+      console.log(this.characters)
+    } );
   }
 }
