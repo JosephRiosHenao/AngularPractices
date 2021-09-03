b55cfd96aee4aa470dd25ac96a758b24bbe8a9e2
Joseph Rios Henao <joseph232005@gmail.com>
2021-09-03 11:17:51 -0500

    init proyect

---
+++ practice19/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBl4BwGY2dKqDPx8cIGtX3z3AeIBJMw_nM",
+    authDomain: "controlclients-5d2b0.firebaseapp.com",
+    projectId: "controlclients-5d2b0",
+    storageBucket: "controlclients-5d2b0.appspot.com",
+    messagingSenderId: "48862259101",
+    appId: "1:48862259101:web:eef400313be410306d3174"
+  }
+};
