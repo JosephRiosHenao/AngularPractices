208b251fbde2e2fad4cfcff8a73e34e75d74b1e4
Joseph Rios Henao <joseph232005@gmail.com>
2021-11-10 15:47:15 -0500

    continue with desing for input box

--- practice20/src/app/app-routing.module.ts
+++ practice20/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SumComponent } from './sum/sum.component';
 
 const routes: Routes = [
   {path: "home", component: HomeComponent},
+  {path: "sum", component: SumComponent},
   {path: "**", component: HomeComponent }
 ];
 
