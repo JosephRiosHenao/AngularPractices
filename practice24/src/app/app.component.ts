9d9e72a0536b461b75396a6b78035ed5e7956317
Joseph Rios Henao <joseph232005@gmail.com>
2021-12-08 15:14:42 -0500

    templeate finish

--- practice24/src/app/app.component.ts
+++ practice24/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'practice24';
+
+  public formWork:FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    yearsOld: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+    tel: new FormControl('', Validators.required),
+    genre: new FormControl('', Validators.required),
+  });
+
+  public formSon:FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    yearsOld: new FormControl('', Validators.required),
+    activity: new FormControl('', Validators.required),
+  })
+
+  yearsLast:number = 0;
+
+
 }
