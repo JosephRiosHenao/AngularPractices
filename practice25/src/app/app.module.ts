980ab47c74a5b6677536d7cfaee05d42577c94f4
Joseph Rios Henao <joseph232005@gmail.com>
2022-01-25 05:33:44 -0500

    litle changes

--- practice25/src/app/app.module.ts
+++ practice25/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { NgParticlesModule } from "ng-particles";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgParticlesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
